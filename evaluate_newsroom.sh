f8fada221e9bc82f26a2c2d022f68caea7d64aaa
ecly <ecly@mailbox.org>
2019-02-27 08:52:44 +0100

    update train_newsroom.sh and add evaluate_newsroom.sh

---
+++ evaluate_newsroom.sh
@@ -0,0 +1,1 @@
+python run_summarization.py --mode=decode --data_path=./data/test.tsv --vocab_path=./data/vocab --log_root=./log --exp_name=newsroom --is_newsroom=true --single_pass=1
--- train_newsroom.sh
+++ train_newsroom.sh
@@ -1,1 +1,1 @@
-python run_summarization.py --mode=train --data_path=./data/dev.tsv --vocab_path=./data/vocab --log_root=./log --exp_name=newsroom --is_newsroom=true --max_steps=100000
+python run_summarization.py --mode=train --data_path=./data/train.tsv --vocab_path=./data/vocab --log_root=./log --exp_name=newsroom --is_newsroom=true --max_steps=100000
