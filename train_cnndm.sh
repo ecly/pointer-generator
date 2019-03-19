4076c89ff3ec3d0a8ac13d73f2fe3860ef0891ae
ecly <ecly@mailbox.org>
2019-03-19 14:24:53 +0100

    add commands for converting models to coverage and training them

--- train_cnndm.sh
+++ train_cnndm.sh
@@ -1,2 +1,10 @@
-# we still use is_newsroom=true to handle our own preprocessed input
-python run_summarization.py --mode=train --data_path=./data/cnndm_train.tsv --vocab_path=./data/cnndm.vocab --log_root=./log --exp_name=cnndm --is_newsroom=true --max_steps=230000
+# We still use is_newsroom=true to handle our own preprocessed input
+
+# Train for the initial 230_000 iterations without coverage
+# python run_summarization.py --mode=train --data_path=./data/cnndm_train.tsv --vocab_path=./data/cnndm.vocab --log_root=./log --exp_name=cnndm --is_newsroom=true --max_steps=230000
+
+# Convert the model to a coverage version
+# python run_summarization.py --mode=train --data_path=./data/cnndm_train.tsv --vocab_path=./data/cnndm.vocab --log_root=./log --exp_name=cnndm --is_newsroom=true --max_steps=233000 --convert_to_coverage=True --coverage=True
+
+# Train the coverage enabled model for 3000 iterations
+python run_summarization.py --mode=train --data_path=./data/cnndm_train.tsv --vocab_path=./data/cnndm.vocab --log_root=./log --exp_name=cnndm --is_newsroom=true --max_steps=233000 --coverage=True
--- train_newsroom.sh
+++ train_newsroom.sh
@@ -1,1 +1,8 @@
-python run_summarization.py --mode=train --data_path=./data/newsroom_train.tsv --vocab_path=./data/newsroom.vocab --log_root=./log --exp_name=newsroom --is_newsroom=true --max_steps=230000
+# Train for the initial 230_000 iterations without coverage
+# python run_summarization.py --mode=train --data_path=./data/newsroom_train.tsv --vocab_path=./data/newsroom.vocab --log_root=./log --exp_name=newsroom --is_newsroom=true --max_steps=230000
+
+# Convert the model to a coverage version
+# python run_summarization.py --mode=train --data_path=./data/newsroom_train.tsv --vocab_path=./data/newsroom.vocab --log_root=./log --exp_name=newsroom --is_newsroom=true --max_steps=233000 --convert_to_coverage=True --coverage=True
+
+# Train the coverage enabled model for 3000 iterations
+python run_summarization.py --mode=train --data_path=./data/newsroom_train.tsv --vocab_path=./data/newsroom.vocab --log_root=./log --exp_name=newsroom --is_newsroom=true --max_steps=233000 --coverage=True
