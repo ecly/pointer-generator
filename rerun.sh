342e99113c2459633479c51154d0735d82dfcd39
ecly <ecly@mailbox.org>
2019-04-24 22:12:42 +0200

    update bash script for reproducing

--- rerun.sh
+++ rerun.sh
@@ -1,13 +1,14 @@
-### Iteration handling is missing (and currently not supported by the code)
-
 # Train initial Pointer Generator model
 python run_summarization.py --mode=train --data_path=data/chunked/train_* --vocab_path=data/vocab --log_root=log --exp_name=rerun --iterations 230000
 
 # Evaluate Pointer Generator
-python run_summarization.py --mode=decode --data_path=data//chunked/test_* --vocab_path=data/vocab --log_root=/log --exp_name=rerun --single_pass=True
+python run_summarization.py --mode=decode --data_path=data/chunked/test_* --vocab_path=data/vocab --log_root=log --exp_name=rerun --single_pass=True
 
 # Convert model to coverage
-python run_summarization.py --logdir=log --exp_name=rerun --covert_to_coverage=True --coverage=True
+python run_summarization.py --log_root=log --exp_name=rerun --covert_to_coverage=True --coverage=True
 
 # Convert model to coverage
-python run_summarization.py --mode=train --logdir=log --exp_name=rerun --covert_to_coverage=True --iterations 233000 --coverage=True
+python run_summarization.py --mode=train --log_root=log --exp_name=rerun --iterations 233000 --coverage=True
+
+# Evaluate Pointer Generator + Coverage
+python run_summarization.py --mode=decode --data_path=data/chunked/test_* --vocab_path=data/vocab --log_root=log --exp_name=rerun --single_pass=True --coverage=True
