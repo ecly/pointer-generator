d068cf8fe08f1149613ea6ff30354bd2968eece0
ecly <ecly@mailbox.org>
2019-04-25 09:46:04 +0200

    adjust errors in rerun.sh

--- rerun.sh
+++ rerun.sh
@@ -5,10 +5,10 @@ python run_summarization.py --mode=train --data_path=data/chunked/train_* --voca
 python run_summarization.py --mode=decode --data_path=data/chunked/test_* --vocab_path=data/vocab --log_root=log --exp_name=rerun --single_pass=True
 
 # Convert model to coverage
-python run_summarization.py --log_root=log --exp_name=rerun --covert_to_coverage=True --coverage=True
+python run_summarization.py --vocab_path=data/vocab --log_root=log --exp_name=rerun --convert_to_coverage=True --coverage=True
 
-# Convert model to coverage
-python run_summarization.py --mode=train --log_root=log --exp_name=rerun --iterations 233000 --coverage=True
+# Train model for 3000 iterations with coverage
+python run_summarization.py --vocab_path=data/vocab --mode=train --log_root=log --exp_name=rerun --iterations 233000 --coverage=True --data_path=data/chunked/train_*
 
 # Evaluate Pointer Generator + Coverage
 python run_summarization.py --mode=decode --data_path=data/chunked/test_* --vocab_path=data/vocab --log_root=log --exp_name=rerun --single_pass=True --coverage=True
